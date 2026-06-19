62c2202b872fd270588ad6efd5a59274cec3d432
S Mendoza <samuelmendozal06@gmail.com>
2026-06-18 19:00:17 -0500

    Supabase config

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@
    ⚠️  Reemplaza los valores con los de tu proyecto en Supabase
    ============================================================ */
 
-const SUPABASE_URL  = 'https://TUPROYECTO.supabase.co';   // <-- cambia esto
-const SUPABASE_ANON = 'TU_ANON_KEY';                       // <-- cambia esto
+const SUPABASE_URL  = 'https://dqolmpchfaivtkdspldu.supabase.co/rest/v1/';   // <-- cambia esto
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxb2xtcGNoZmFpdnRrZHNwbGR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4MTQ0MDMsImV4cCI6MjA5NzM5MDQwM30.gKWLa_5eDpv2XQ6kb3ez3zYS_zfQY1hKkjJcfsefwu4';                       // <-- cambia esto
